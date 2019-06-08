1ce45f9a3091faa221af8755af2dd8f94e7efabd
AnielleMacedo <36572573+AnielleMacedo@users.noreply.github.com>
2019-06-08 16:41:18 +0200

    Exercicio Validacao de login e senha

---
+++ ExerciseZimuel/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Tela de Login</title>
+</head>
+<body>
+
+	<!--crio o form e em action coloco o nome do arquivo php o qual irei mandar os dados deste file index.html. A variavel name è importante pois sera usada no codigo php p poder collegaarsi-->
+	<form action="login.php" method="POST" />
+	User: <input type="text" name="email" />	
+	Password: <input type="password" name="senha" />
+	<!--crio um button que ira enviar oa dados-->
+	<input type="submit" value="Enter" />
+
+	</form>
+
+</body>
+</html>
